e3c1e81275fa23805d9e4df71b2645f30a07a153
VictorMora2001 <al059569@uacam.mx>
2024-05-16 18:55:17 -0600

    first commit

--- greeting-client.py
+++ greeting-client.py
@@ -1,7 +1,9 @@
 # saved as greeting-client.py
 import Pyro4
 
+ipAdressServer ="192.168.156.39"
+
 name = input("¿Cuál es tu nombre? ").strip()
 
-greeting_maker = Pyro4.Proxy("PYRONAME:example.greeting")    # use name server object lookup uri shortcut
-print(greeting_maker.get_fortune(name))
+greetingMaker = Pyro4.core.Proxy('PYRO:Greeting@' + ipAdressServer + ':9090')    # use name server object lookup uri shortcut
+print(greetingMaker.get_fortune(name))
--- greeting-server.py
+++ greeting-server.py
@@ -5,12 +5,8 @@ import Pyro4
 class GreetingMaker(object):
     def get_fortune(self, name):
         return "Hola, {0}. Aquí tienes tu mensaje de la fortuna:\n" \
-               "Tu número de la suerte de mañana es el: 105642, que tengas un excelente día.".format(name)
+               "Tu número de la suerte de mañana es el: 10234, que tengas un excelente día.".format(name)
 
-daemon = Pyro4.Daemon()                # make a Pyro daemon
-ns = Pyro4.locateNS()                  # find the name server
-uri = daemon.register(GreetingMaker)   # register the greeting maker as a Pyro object
-ns.register("example.greeting", uri)   # register the object with a name in the name server
-
-print("Ready.")
-daemon.requestLoop()                   # start the event loop of the server to wait for calls
+Pyro4.Daemon.serveSimple({
+    GreetingMaker: "Greeting",
+}, host="0.0.0.0" , port=9090, ns=False, verbose=True)
